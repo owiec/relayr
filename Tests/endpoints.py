4dbf788b3eab47db42ee84ece4662b5296e69c53
owiec <owiecpl@gmail.com>
2020-01-26 12:28:12 +0100

    Remove pointless f-string

--- Tests/endpoints.py
+++ Tests/endpoints.py
@@ -3,7 +3,7 @@ import variables
 root = 'http://ws.audioscrobbler.com/2.0/'
 api_key = f'&api_key={variables.API_KEY}'
 json = '&format=json'
-limit = f'&limit='
+limit = '&limit='
 
 chart = f'{root}?method=chart.'
 top_artist = f'{chart}getTopArtists{api_key}'
